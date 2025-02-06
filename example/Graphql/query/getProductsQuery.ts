6e0bc56a670032f4d84ff9496120b85e7e6c8f7b
Aleksandr Kononov <oleksandr.kononov@devit.group>
2025-02-06 13:45:22 +0200

    add gql getProducts query

---
+++ example/Graphql/query/getProductsQuery.ts
@@ -0,0 +1,92 @@
+//@ts-nocheck
+
+//Loader example
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+  const { admin } = await authenticate.admin(request);
+
+  const url = new URL(request.url);
+
+  const afterId = url.searchParams.get('afterId');
+  const beforeId = url.searchParams.get('beforeId');
+
+  const res = await getProductsQuery({
+    first: beforeId ? null : MAX_COUNT,
+    last: beforeId ? MAX_COUNT : null,
+    afterId,
+    beforeId,
+    gql: admin.graphql,
+  });
+
+  const json = await res.json();
+
+  return { data: json?.data as Response }; //yayayayayayaya
+};
+
+//Query example
+import { AdminApiContextWithRest } from 'node_modules/@shopify/shopify-app-remix/dist/ts/server/clients';
+
+type GetProducts = {
+  gql: AdminApiContextWithRest['graphql'];
+  first: number | null;
+  last: number | null;
+  afterId: string | null;
+  beforeId: string | null;
+};
+
+export const getProductsQuery = ({
+  gql,
+  first,
+  last,
+  afterId = null,
+  beforeId = null,
+}: GetProducts) => {
+  const data = gql(
+    `#graphql
+  query inventoryItems($first: Int, $last: Int, $afterId: String, $beforeId: String) {
+    productsCount {
+      count
+    }
+    products (first: $first,
+    last: $last,
+    after: $afterId,
+    before: $beforeId) {
+      pageInfo {
+      endCursor
+      startCursor
+      hasNextPage
+      hasPreviousPage
+    }
+    edges {
+			 node {
+        id
+        title
+        handle
+        totalInventory
+      	contextualPricing (context: {country: UA (UA/US/CA etc.)}) {
+				priceRange {
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+        }
+        category {
+          id
+					name
+        }
+      }
+     }
+    }
+  }`,
+    {
+      variables: {
+        first,
+        last,
+        afterId,
+        beforeId,
+      },
+    }
+  );
+
+  return data;
+};
